107fe5630040e9370433644b37764905e1351deb
Leonardo <lmarciano@alunos.utfpr.edu.br>
2019-12-19 12:58:47 -0300

    add font

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -32,6 +32,7 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+import Font from '@ckeditor/ckeditor5-font/src/font';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -61,7 +62,8 @@ ClassicEditor.builtinPlugins = [
 	Table,
 	TableToolbar,
 	Alignment,
-	SimpleUploadAdapter
+	SimpleUploadAdapter,
+	Font
 ];
 
 // Editor configuration.
